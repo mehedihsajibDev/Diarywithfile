7c91e552e4ad6b1ca34d239f20f2a55344d2df49
unknown <you@example.com>
2018-12-08 14:51:02 +0600

    project is ready

---
+++ app/src/main/java/com/example/sajib/diarywithfile/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.sajib.diarywithfile;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editid);
+        button = findViewById(R.id.buttonid);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                if (text != null) {
+                    writemethod(text);
+                } else {
+                    Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        writemethod();
+    }
+
+    private void writemethod(String text) {
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("diary.txt", Context.MODE_PRIVATE);
+            fileOutputStream.write(text.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(this, "successfully saved", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writemethod() {
+        try {
+            FileInputStream fileInputStream = openFileInput("diary.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line + "\n");
+            }
+            editText.setText(stringBuffer.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
